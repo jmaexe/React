b48eadcb94d1d8d50341edadfe6963f478c5876b
jmxexe <miccijhonandrea@gmail.com>
2024-08-03 14:32:51 +0200

    add server marvel

---
+++ film-project/server/marvel/controllers/charactersController.js
@@ -0,0 +1,45 @@
+const md5 = require('md5');
+const ts = 1;
+
+//aggiungere limit
+const getAllCharacters = async (req, res) => {
+  const response = await fetch(
+    `${process.env.BASE_URL}/characters?limit=5&apikey=${
+      process.env.PUBLIC_KEY
+    }&hash=${md5(
+      ts + process.env.PRIVATE_KEY + process.env.PUBLIC_KEY
+    )}&ts=${ts}`
+  );
+  const jsonData = await response.json();
+
+  if (jsonData.code == 200 && jsonData.status == 'Ok') {
+    const { results } = jsonData.data;
+    res.json(results);
+  } else {
+    res.send('error fetching data characters');
+  }
+};
+
+const getCharacterByName = async (req, res) => {
+  const { name } = req.params;
+
+  const response = await fetch(
+    `${process.env.BASE_URL}/characters?nameStartsWith=${name}&apikey=${
+      process.env.PUBLIC_KEY
+    }&hash=${md5(
+      ts + process.env.PRIVATE_KEY + process.env.PUBLIC_KEY
+    )}&ts=${ts}`
+  );
+  const jsonData = await response.json();
+
+  if (jsonData.code == 200 && jsonData.status == 'Ok') {
+    res.json(jsonData);
+  } else {
+    res.send('error fetching data character: ' + name);
+  }
+};
+
+module.exports = {
+  getAllCharacters,
+  getCharacterByName,
+};
